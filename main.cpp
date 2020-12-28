d9507f1c3117cad0dafbfa90eedb026072fa9104
Vincent Schmid <puterich@gmail.com>
2020-12-28 09:11:04 +0100

    first working test

---
+++ main.cpp
@@ -0,0 +1,122 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - Basic 3d example
+*
+*   Welcome to raylib!
+*
+*   To compile example, just press F5.
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*   raylib official webpage: www.raylib.com
+*
+*   Enjoy using raylib. :)
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2013-2020 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+#include "raylib.h"
+#include "raymath.h"
+#include "rlgl.h"           // raylib OpenGL abstraction layer to OpenGL 1.1, 2.1, 3.3+ or ES2
+
+#define RLIGHTS_IMPLEMENTATION
+#include "rlight.h"
+
+int main() 
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib");
+
+    int val = 0;
+
+    // Load model
+    Model boat = LoadModel("assets/sailboat/sailBoat.obj");
+    Texture texture = LoadTexture("assets/texel_checker.png");
+
+    Mesh myPlaneMesh = GenMeshPlane(10, 10, 5, 5);
+    Model myPlane = LoadModelFromMesh(myPlaneMesh);
+
+    boat.materials[0].maps[MAP_DIFFUSE].texture = texture;
+    myPlane.materials[0].maps[MAP_DIFFUSE].texture = texture;
+
+    Camera camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 60.0f;
+    camera.type = CAMERA_PERSPECTIVE;
+
+    // Load shader and set up some uniforms
+    Shader shader = LoadShader("data/base_lighting.vs", "data/lighting.fs");
+    shader.locs[LOC_MATRIX_MODEL] = GetShaderLocation(shader, "matModel");
+    shader.locs[LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
+
+    // Ambient light level
+    int ambientLoc = GetShaderLocation(shader, "ambient");
+    SetShaderValue(shader, ambientLoc, (float[4]){ 0.2f, 0.2f, 0.2f, 1.0f }, UNIFORM_VEC4);
+
+    Vector3 position = { 0 };
+
+    boat.materials[0].shader = shader;
+
+    // Using just 1 point lights
+    CreateLight(LIGHT_POINT, (Vector3){ 0, 2, 6 }, Vector3Zero(), WHITE, shader);
+
+    SetCameraMode(camera, CAMERA_THIRD_PERSON);
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera);
+
+        myPlaneMesh.vertices[1] = (float)(val++ % 20);
+
+        rlUpdateMesh(myPlaneMesh, 0, myPlaneMesh.vertexCount);
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            // Update the light shader with the camera view position
+            SetShaderValue(shader, shader.locs[LOC_VECTOR_VIEW], &camera.position.x, UNIFORM_VEC3);
+
+            BeginMode3D(camera);
+
+                DrawModel(myPlane, position, 1.0f, WHITE);
+                // DrawModel(boat, position, 1.0f, WHITE);
+
+            EndMode3D();
+
+            DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
+
+            DrawFPS(10, 10);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    UnloadModel(boat);
+    UnloadTexture(texture);
+    UnloadShader(shader);
+    
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
