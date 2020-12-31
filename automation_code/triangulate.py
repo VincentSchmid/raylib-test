e7c086c36b3e7f47c2e5469bbb0117cb2f86bf59
Vincent Schmid <35878596+VincentSchmid@users.noreply.github.com>
2020-12-31 15:58:50 +0100

    Update triangulate.py

--- automation_code/triangulate.py
+++ automation_code/triangulate.py
@@ -9,28 +9,12 @@ to run: have Blender in path variables
 replaces the paths in the variable below
 and run Blender --background --python traingulate.py
 
-this script assumes only one object per obj file,
-if that is not the case 
-this line needs to change:
-obj = obj_import_mesh(file)[0]
+trangulation happens during the export step with:
+use_triangles=True
 """
 
 import_mesh_folder = "/Users/vincentschmid/Documents/coding/games/testGame/assets/3DNaturePack/Models"
 export_mesh_folder = "/Users/vincentschmid/Documents/coding/games/raylib-game/assets/3DNaturePack/Models"
-
-
-def get_all_objects_in_scene():
-    return [obj for obj in bpy.data.objects]
-
-
-def get_new_objects_in_scene(old_objects):
-    current_objects = get_all_objects_in_scene()
-    new_objects = []
-    for obj in current_objects:
-        if obj not in old_objects:
-            new_objects.append(obj)
-
-    return new_objects
 
 
 def clearScene():
@@ -38,11 +22,9 @@ def clearScene():
 
 
 def obj_import_mesh(filepath):
-    objs = get_all_objects_in_scene()
     bpy.ops.import_scene.obj(
         filepath=str(Path(filepath).resolve())
     )
-    return get_new_objects_in_scene(objs)
 
 
 def obj_export_scene(filepath):
@@ -55,6 +37,6 @@ def obj_export_scene(filepath):
 
 for file in glob.glob(import_mesh_folder + "/*.obj"):
     export_file = Path(export_mesh_folder).joinpath(Path(file).name)
-    obj = obj_import_mesh(file)[0]
+    obj_import_mesh(file)
     obj_export_scene(export_file)
     clearScene()
