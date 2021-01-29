fd925e306a4fa74ab08245907324987f31e18515
Vincent Schmid <puterich@gmail.com>
2021-01-29 13:55:59 +0100

    automation code exports to glb and excepts parameters

--- automation_code/triangulate.py
+++ automation_code/triangulate.py
@@ -1,3 +1,4 @@
+from sys import argv
 import bpy
 import glob
 from pathlib import Path
@@ -7,15 +8,17 @@ from subprocess import call
 Script to triangulate meshes using Blender
 to run: have Blender in path variables
 replaces the paths in the variable below
-and run Blender --background --python traingulate.py
+and run Blender --background --python traingulate.py <folder of source meshes> <export folder>
 
 trangulation happens during the export step with:
 use_triangles=True
 """
 
-import_mesh_folder = "/Users/vincentschmid/Documents/coding/games/testGame/assets/3DNaturePack/Models"
-export_mesh_folder = "/Users/vincentschmid/Documents/coding/games/raylib-game/assets/3DNaturePack/Models"
+import_mesh_folder = str(Path(argv[-2]).resolve()) # "/Users/vincentschmid/Documents/coding/games/testGame/assets/3DNaturePack/Models"
+export_mesh_folder = str(Path(argv[-1]).resolve()) # "/Users/vincentschmid/Documents/coding/games/raylib-game/assets/3DNaturePack/Models"
 
+print(import_mesh_folder)
+print(export_mesh_folder)
 
 def clearScene():
     bpy.ops.wm.read_homefile(use_empty=True)
@@ -35,8 +38,15 @@ def obj_export_scene(filepath):
     )
 
 
+def glb_export_scene(filepath):
+    bpy.ops.export_scene.gltf(export_format='GLB', 
+        export_texture_dir='', export_colors=True, 
+        export_selected=False, export_yup=False, 
+        filepath=str(Path(filepath).resolve()))
+
+
 for file in glob.glob(import_mesh_folder + "/*.obj"):
-    export_file = Path(export_mesh_folder).joinpath(Path(file).name)
+    export_file = Path(export_mesh_folder).joinpath(Path(file).stem)
     obj_import_mesh(file)
-    obj_export_scene(export_file)
+    glb_export_scene(export_file)
     clearScene()
