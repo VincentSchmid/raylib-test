7a5b003afb960d40c63632b73f57bfbbfc5c7fed
Vincent Schmid <puterich@gmail.com>
2021-01-30 01:59:07 +0100

    fixed typo

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,3 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic 3d example
-*
-*   Welcome to raylib!
-*
-*   To compile example, just press F5.
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2020 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
 #include "raylib.h"
 #include "raymath.h"
 #include "rlgl.h"           // raylib OpenGL abstraction layer to OpenGL 1.1, 2.1, 3.3+ or ES2
@@ -37,7 +17,7 @@ int main()
     // Initialization
     //--------------------------------------------------------------------------------------
     // Load Game Parameters
-    ameSettings* settings = GameSettings::getInstance();
+    GameSettings* settings = GameSettings::getInstance();
     InitWindow(settings->screenWidth, settings->screenHeight, "raylib");
     
     // Generate Map
